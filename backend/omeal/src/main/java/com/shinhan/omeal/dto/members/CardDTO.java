19ba13815264ad1a7ea9ea708f2e3abd66d5f770
djdjdddd <djdj2297@gmail.com>
2023-08-01 15:57:57 +0900

    feat(BE) : CardDTO 필드 추가

--- backend/omeal/src/main/java/com/shinhan/omeal/dto/members/CardDTO.java
+++ backend/omeal/src/main/java/com/shinhan/omeal/dto/members/CardDTO.java
@@ -2,10 +2,17 @@ package com.shinhan.omeal.dto.members;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.sql.Date;
 
 @Getter
 @Setter
+@ToString
 public class CardDTO {
+    //private String cardBrand;
+    private String serialNumber; // 카드번호
+    private String expiryDate; // 신용카드의 유효기간 영어 참조 : https://owldictionary.com/best-before-date/
+    private Integer cvc;
+    private Integer cardPwd;
 }
