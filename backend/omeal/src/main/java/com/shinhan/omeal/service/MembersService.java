2ffab57a4940b435f85acebb7fc618c1984522e0
djdjdddd <djdj2297@gmail.com>
2023-08-01 17:43:38 +0900

    feat(BE) : MemberService, signUp 메소드 추가

--- backend/omeal/src/main/java/com/shinhan/omeal/service/MembersService.java
+++ backend/omeal/src/main/java/com/shinhan/omeal/service/MembersService.java
@@ -6,17 +6,34 @@ import com.shinhan.omeal.dto.members.ResultUserInfoDTO;
 import com.shinhan.omeal.entity.Members;
 import com.shinhan.omeal.repository.MembersRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MembersService {
 
     final MembersRepository memRepo;
 
+    // 카드정보 입력 후 회원가입 완료 버튼 눌렀을 때
     public void signUp(CardDTO cardDto){
+        log.info("Date 형식으로 바꿔줄 놈 : "+cardDto.getExpiryDate());
+        String expiryDate = cardDto.getExpiryDate();
+        log.info(String.valueOf(expiryDate));
+        String[] arr = expiryDate.split("/");
+        String month = arr[0];
+        String year = "20" + arr[1];
+        LocalDate ld = LocalDate.parse(year+month, DateTimeFormatter.ofPattern("yyyy/MM"));
+        log.info(String.valueOf(ld));
+        Integer cvc = cardDto.getCvc();
+        Integer cardPwd = cardDto.getCardPwd();
 
+        //cRepo.save();
     }
 
     @Transactional
