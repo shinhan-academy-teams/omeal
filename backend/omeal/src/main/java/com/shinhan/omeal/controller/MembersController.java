330981ad02a50ae2b79764f0aba4955799332d14
djdjdddd <djdj2297@gmail.com>
2023-08-01 16:29:30 +0900

    feat(BE) : 회원가입(signUp) 메소드 추가

--- backend/omeal/src/main/java/com/shinhan/omeal/controller/MembersController.java
+++ backend/omeal/src/main/java/com/shinhan/omeal/controller/MembersController.java
@@ -1,11 +1,27 @@
 package com.shinhan.omeal.controller;
 
+import com.shinhan.omeal.dto.members.CardDTO;
+import com.shinhan.omeal.service.MembersService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@Slf4j
 public class MembersController {
 
+    @Autowired
+    MembersService mService;
 
+    @PostMapping(value = "/signup", consumes = "application/json", produces = "text/plain;charset=utf-8")
+    public String signUp(@RequestBody CardDTO cardDto) {
+        log.info("회원 가입 완료 클릭 : " + cardDto.toString());
+        mService.signUp(cardDto);
+
+        return null;
+    }
 }
