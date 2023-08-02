46259d5d55ec87f6f999a2320274643aca542dda
eho <psj9258@gmail.com>
2023-08-02 16:55:13 +0900

    refactor(FE) : header 로그인 버튼 추가 및 디자인 수정 중

--- frontend/omeal/src/component/Header.js
+++ frontend/omeal/src/component/Header.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import DehazeIcon from "@mui/icons-material/Dehaze";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
@@ -11,6 +10,11 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
+import { Button, Link } from "@mui/material";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+
+import logoImg from "../assets/img/test.png";
 
 function Header(props) {
   const [state, setState] = React.useState({
@@ -30,7 +34,9 @@ function Header(props) {
 
   const list = (anchor) => (
     <Box
-      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+      sx={{
+        width: anchor === "top" || anchor === "bottom" ? "auto" : 250,
+      }}
       role="presentation"
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
@@ -53,7 +59,16 @@ function Header(props) {
   return (
     <>
       <div className="nav">
-        <h1>로고</h1>
+        <img alt="" src={logoImg} width={"30%"} style={{ float: "left" }}></img>
+
+        {/* hidden={!userId} 로그인 되면 버튼 사라짐 */}
+
+        <Button variant="contained">로그인</Button>
+
+        <div>
+          <NotificationsNoneIcon />
+          <AccountCircleIcon />
+        </div>
 
         {["right"].map((anchor) => (
           <React.Fragment key={anchor}>
