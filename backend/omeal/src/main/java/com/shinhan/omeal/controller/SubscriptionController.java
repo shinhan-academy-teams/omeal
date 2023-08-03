049a27f60a0bb415cd4c6eeef20ee86fe73fe5a0
0seony <yseon406@gmail.com>
2023-08-03 14:40:34 +0900

    feat(BE) : SUB009 구독 정보 저장 기능 구현 완료
    SUBSCRIBE 테이블에 구독 데이터 저장

--- backend/omeal/src/main/java/com/shinhan/omeal/controller/SubscriptionController.java
+++ backend/omeal/src/main/java/com/shinhan/omeal/controller/SubscriptionController.java
@@ -1,7 +1,28 @@
 package com.shinhan.omeal.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.shinhan.omeal.dto.subscription.SubscriptionDTO;
+import com.shinhan.omeal.service.SubscriptionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 public class SubscriptionController {
+
+    private final SubscriptionService subscriptionService;
+
+    // 구독 정보 저장
+    @PostMapping(value = "/subscribe")
+    public String subscribe(@RequestBody SubscriptionDTO subscriptionInfo) {
+        return subscriptionService.subscribe(subscriptionInfo);
+    }
+
+    /*
+    Enum 반환
+    1. 배송 주기 선택 SubscriptionType
+    2. 배송 용기 선택 DeliveryContainer
+    3. 음식 카테고리 선택 SubscriptoinCategory
+    4. 배송 시간 선택 DeliveryTime
+     */
+
 }
