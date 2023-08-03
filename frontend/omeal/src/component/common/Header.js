e7935e064b23f9c5c25b87ceb0b94703934ffc7b
zzahee <zzahee366@gmail.com>
2023-08-03 12:09:06 +0900

    style(FE) : 불필요한 import 삭제

--- frontend/omeal/src/component/common/Header.js
+++ frontend/omeal/src/component/common/Header.js
@@ -10,7 +10,7 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
-import { Button, Link } from "@mui/material";
+import { Button } from "@mui/material";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
