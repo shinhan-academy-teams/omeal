4cf95dd169f29024f7c5ca4505f2707e8f5a0bbe
0seony <yseon406@gmail.com>
2023-08-05 20:04:24 +0900

    feat(BE) : TM001 배송시간에 따른 배송현황 업데이트 스케줄러 구현

--- backend/omeal/src/main/java/com/shinhan/omeal/service/TodayMealService.java
+++ backend/omeal/src/main/java/com/shinhan/omeal/service/TodayMealService.java
@@ -38,8 +38,8 @@ public class TodayMealService {
 
     /*
     오늘의 메뉴 준비 스케줄러
-    오전6시에 일괄적으로 배송 준비
-    아침 구독은 배송 시작
+    오전6시에 일괄적으로 배송준비
+    아침구독 배송시작
      */
     @Scheduled(cron = "0 0 6 * * *")
     @Transactional
@@ -52,13 +52,50 @@ public class TodayMealService {
             if(subscription.getMember().getMemberAllergy().size()!=0) {
                 allMenu = menuRepo.findByAllergyNotIn(subscription.getMember().getMemberAllergy());
             }
-            // 메뉴 선정
+            // 메뉴선정
             String todayMeal = getRandomMenu(allMenu);
-            // 배송 준비
+            // 배송준비
             prepareDelivery(subscription, todayMeal);
         });
 
     }
+
+    /*
+    오전8시 1차 배송 확인 스케줄러
+    아침구독 배송완료
+    점심구독 배송시작
+     */
+    @Scheduled(cron = "0 0 8 * * *")
+    public void morningDelivery() {
+        // 아침구독 배송완료
+        completeDelivery(checkMealTime(DeliveryTime.아침));
+        // 점심구독 배송시작
+        inProgressDelivery(checkMealTime(DeliveryTime.점심));
+    }
+
+    /*
+    오후12시 2차 배송 확인 스케줄러
+    점심구독 배송완료
+    저녁구독 배송시작
+    */
+    @Scheduled(cron = "0 0 12 * * *")
+    public void lunchDelivery() {
+        // 점심구독 배송완료
+        completeDelivery(checkMealTime(DeliveryTime.점심));
+        // 저녁구독 배송시작
+        inProgressDelivery(checkMealTime(DeliveryTime.저녁));
+    }
+
+    /*
+    오후19시 3차 배송 확인 스케줄러
+    저녁구독 배송완료
+    */
+    @Scheduled(cron = "0 0 19 * * *")
+    public void dinnerDelivery() {
+        // 저녁구독 배송완료
+        completeDelivery(checkMealTime(DeliveryTime.저녁));
+    }
+
 
     // 랜덤으로 오늘의 메뉴 선정
     private String getRandomMenu(Set<Menu> allMenu){
@@ -78,6 +115,31 @@ public class TodayMealService {
                 .deliveryAddr(subscription.getDeliveryAddr())
                 .build();
         tmRepo.save(todayDelivery);
+    }
+
+    // 멤버별 구독 시간 확인
+    private List<Members> checkMealTime(DeliveryTime time) {
+        List<Members> membersByTime = new ArrayList<>();
+        subRepo.findAllByMealTime(time).forEach(subscription -> {
+            membersByTime.add(subscription.getMember());
+        });
+        return membersByTime;
+    }
+
+    // 배송 현황을 배송중으로 업데이트
+    private void inProgressDelivery(List<Members> membersByTime) {
+        tmRepo.findAllByMemberIn(membersByTime).forEach(history->{
+            history.updateDeliveryStatus(DeliveryStatus.배송중);
+            tmRepo.save(history);
+        });
+    }
+
+    // 배송 현황을 배송완료로 업데이트
+    private void completeDelivery(List<Members> membersByTime) {
+        tmRepo.findAllByMemberIn(membersByTime).forEach(history->{
+            history.updateDeliveryStatus(DeliveryStatus.배송완료);
+            tmRepo.save(history);
+        });
     }
 
 }
