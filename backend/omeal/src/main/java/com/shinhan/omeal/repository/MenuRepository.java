76cce74958656e55fc45506599361a78676e710d
0seony <yseon406@gmail.com>
2023-08-05 19:05:07 +0900

    feat(BE) : 구독 타입별로 메뉴 데이터 조회를 위한 findBy 메서드 추가

--- backend/omeal/src/main/java/com/shinhan/omeal/repository/MenuRepository.java
+++ backend/omeal/src/main/java/com/shinhan/omeal/repository/MenuRepository.java
@@ -1,5 +1,6 @@
 package com.shinhan.omeal.repository;
 
+import com.shinhan.omeal.dto.subscription.SubscriptionCategory;
 import com.shinhan.omeal.entity.Allergy;
 import com.shinhan.omeal.entity.Menu;
 import org.springframework.data.jpa.repository.Query;
@@ -14,4 +15,7 @@ public interface MenuRepository extends CrudRepository<Menu, Long> {
     // findByAgeNotIn(Collection<Age> ages) 형태 이용함
     // ★중복된 메뉴이름이 많고, 여러 건이 나와서 List가 아니라 Set으로 담음.
     Set<Menu> findByAllergyNotIn(List<Allergy> allergyList); // 참조 : https://docs.spring.io/spring-data/jpa/docs/2.5.1/reference/html/#jpa.query-methods
+
+    // 구독 카테고리별 메뉴
+    Set<Menu> findByCategory(SubscriptionCategory category);
 }
