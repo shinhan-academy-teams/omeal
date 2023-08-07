373879811d78d270644693fc8e2f2d94cad075dd
eho <psj9258@gmail.com>
2023-08-07 14:37:12 +0900

    refactor(FE) : 글 작성 기능 수정 중 (이미지 파일만 완료하면 됨)

--- frontend/omeal/src/component/community/Register.js
+++ frontend/omeal/src/component/community/Register.js
@@ -1,60 +1,154 @@
-import { Box, Button, Chip, Stack, TextField } from "@mui/material";
-import React from "react";
-import { Navigate } from "react-router-dom";
+import {
+  Box,
+  Button,
+  Chip,
+  FormControl,
+  FormControlLabel,
+  RadioGroup,
+  TextField,
+} from "@mui/material";
+import axios from "axios";
+import React, { useState } from "react";
+import { useRecoilValue } from "recoil";
+import { SignInState } from "../../recoil/SignInState";
 
 function Register(props) {
-  const handleClick = (chipValue) => {
-    console.log(chipValue);
+  const memberId = useRecoilValue(SignInState);
+  const [inputTitle, setInputTitle] = useState("");
+  const [inputContents, setInputContents] = useState("");
+  //const [inputPhoto, setInputPhoto] = [useState("")];
+
+  // 토글
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const handleInputTitle = (e) => {
+    setInputTitle(e.target.value);
+  };
+  const handleInputContents = (e) => {
+    setInputContents(e.target.value);
   };
 
-  const [value, setValue] = React.useState(0);
+  const changeOption = (value) => {
+    setSelectedOption(value);
+    console.log(selectedOption);
+  };
+  const radioOptions = [
+    {
+      value: "자유게시판",
+      label: "자유게시판",
+    },
+    {
+      value: "질문/답변",
+      label: "질문/답변",
+    },
+    {
+      value: "오늘의 밀",
+      label: "오늘의 밀",
+    },
+    {
+      value: "맛집 추천",
+      label: "맛집 추천",
+    },
+    {
+      value: "인기글",
+      label: "인기글",
+    },
+  ];
+
+  const onClickRegister = (e) => {
+    axios({
+      method: "post",
+      url: "/board/register",
+      data: JSON.stringify({
+        member: { memberId: memberId },
+        title: inputTitle,
+        content: inputContents,
+        // photo: inputPhoto,
+        category: selectedOption,
+        townName: "샌드럴파크",
+      }),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((r) => {})
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <div>
       <Box sx={{ width: "500px" }}>
-        <Stack spacing={3} alignItems="center" sx={{ marginTop: "10px" }}>
-          <Stack direction="row" spacing={1}>
-            {[
-              "자유게시판",
-              "질문/답변",
-              "오늘의 밀",
-              "맛집 추천",
-              "인기글",
-            ].map((chip, index) => (
-              <Chip
-                key={index}
-                label={chip}
-                color="primary"
-                variant="outlined"
-                onClick={() => {
-                  handleClick(chip);
-                }}
-              />
-            ))}
-          </Stack>
-          <Stack direction="column"></Stack>
-        </Stack>
-
-        <TextField sx={{ width: "100%" }} placeholder="글제목 입력하세요" />
+        {/* 토글버튼 */}
+        <FormControl
+          sx={{
+            p: "5px",
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+            alignItems: "center",
+          }}
+        >
+          <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue="female"
+            name="radio-buttons-group"
+            sx={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}
+          >
+            {radioOptions.map((radioOption) => {
+              return (
+                <FormControlLabel
+                  value={radioOption.value}
+                  key={radioOption.value}
+                  control={
+                    <Chip
+                      color="primary"
+                      label={radioOption.label}
+                      variant={
+                        selectedOption === radioOption.value
+                          ? "filled"
+                          : "outlined"
+                      }
+                      sx={{ m: "5px" }}
+                      onClick={() => changeOption(radioOption.value)}
+                    />
+                  }
+                />
+              );
+            })}
+          </RadioGroup>
+        </FormControl>
 
         <TextField
-          sx={{ width: "100%", marginTop: 5 }}
+          sx={{ width: "100%" }}
+          placeholder="글제목 입력하세요"
+          name="title"
+          value={inputTitle}
+          onChange={handleInputTitle}
+        />
+
+        <TextField
+          sx={{ width: "100%", marginTop: 3 }}
           id="outlined-multiline-static"
           multiline
           rows={15}
           placeholder="글내용"
+          value={inputContents}
+          name="content"
+          onChange={handleInputContents}
         />
 
         <TextField
-          sx={{ width: "100%", marginTop: 5 }}
+          sx={{ width: "100%", marginTop: 3 }}
           type="file"
           placeholder="사진파일 업로드"
+          // value={setInputPhoto}
         />
 
         <Button
           variant="contained"
           href="/omealland/register"
           sx={{ marginTop: "30px" }}
+          onClick={onClickRegister}
         >
           글 등록
         </Button>
