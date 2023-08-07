59eec93c61d0a86e37d1b3f6ad148e78e13215a6
Jennorresothie <head7579@gmail.com>
2023-08-07 14:10:57 +0900

    refactor(BE) 인기글 조회, 닉네임 조회 수정

--- backend/omeal/src/main/java/com/shinhan/omeal/controller/BoardController.java
+++ backend/omeal/src/main/java/com/shinhan/omeal/controller/BoardController.java
@@ -38,11 +38,12 @@ public class BoardController {
     // 특정 마을의 카테고리별 글 목록 조회
     @GetMapping("/{townname}/{category}")
     public List<ContentsDTO> getContentsList(@PathVariable TownName townname,@PathVariable BoardCategory category) {
-        if(category.equals("인기글")){
+        if(category==BoardCategory.인기글){
             return boardService.getBestContentsList(townname);
         }
-        return boardService.getContentsCategory(townname,category);
-
+        else{
+            return boardService.getContentsCategory(townname,category);
+        }
     }
 
     // 특정 마을의 제목으로 게시물 조회
@@ -53,8 +54,8 @@ public class BoardController {
 
     // 특정 마을의 닉네임으로 게시물 조회
     @GetMapping("/{townname}/nick-name")
-    public List<ContentsDTO> getNicknameContentsList(@PathVariable TownName townname, String nickName) {
-        return boardService.getNicknameContentsList(townname, nickName);
+    public List<ContentsDTO> getNicknameContentsList(@PathVariable TownName townname, String nickname) {
+        return boardService.getNicknameContentsList(townname, nickname);
     }
 
     // 마을의 내용으로 게시글 조회
