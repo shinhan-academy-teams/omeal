8c734e84673a2d146403c3042074a03b317f7f77
KKangBro <417kky@gmail.com>
2023-08-07 17:08:25 +0900

    feat(BE) : TM001~002 단 건 피드백 기능 완료

--- backend/omeal/src/main/java/com/shinhan/omeal/dto/todayMeal/TodayMealDTO.java
+++ backend/omeal/src/main/java/com/shinhan/omeal/dto/todayMeal/TodayMealDTO.java
@@ -14,8 +14,6 @@ public class TodayMealDTO {
     private String menu; // 배송 메뉴
     private DeliveryStatus status; // 배송 현황
     private String deliveryAddr; // 배송지
-    
-    // 나중에 피드백 관련 DTO 추가할 예정
 
     // toDTO
     public static TodayMealDTO toTodayMealDTO(DeliveryHistory deliveryHistory){
