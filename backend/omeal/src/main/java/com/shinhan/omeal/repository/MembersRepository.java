08ddbe489205326d9854c0f308f6d0ed280aa5a1
Jennorresothie <head7579@gmail.com>
2023-08-07 12:43:46 +0900

    feat(BE) OML003 마을별 닉네임, 내용으로  조회 기능+ refactor(BE) 인기글조회

--- backend/omeal/src/main/java/com/shinhan/omeal/repository/MembersRepository.java
+++ backend/omeal/src/main/java/com/shinhan/omeal/repository/MembersRepository.java
@@ -4,9 +4,12 @@ import com.shinhan.omeal.entity.Members;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MembersRepository extends CrudRepository<Members, String> {
 
     public Members findByMemberNick(String memberNick);
 
+    List<Members> findAllByMemberNickContaining(String memberNick);
 }
