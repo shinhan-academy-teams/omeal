0c7e2e617b2d2fe4053d8bdd542744c68bb550bd
0seony <yseon406@gmail.com>
2023-08-08 20:47:48 +0900

    refactor(FE) : 배송내역 테이블 custom

--- frontend/omeal/src/component/mypage/DeliveryInfo.js
+++ frontend/omeal/src/component/mypage/DeliveryInfo.js
@@ -7,11 +7,20 @@ import {
   TableHead,
   TableRow,
   Typography,
+  tableCellClasses,
 } from "@mui/material";
+import { styled } from "@mui/material/styles";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
 import { SignInState } from "../../recoil/SignInState";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.common.white,
+  },
+}));
 
 function DeliveryInfo(props) {
   // 회원 아이디
@@ -24,7 +33,6 @@ function DeliveryInfo(props) {
         params: { memId: memberId },
       })
       .then(function (response) {
-        console.log(response.data);
         setDeliveryHistory(response.data);
       })
       .catch(function (error) {});
@@ -44,13 +52,13 @@ function DeliveryInfo(props) {
       >
         <Typography variant="h4">배송내역</Typography>
         <TableContainer>
-          <Table sx={{ minWidth: 400 }} aria-label="customized table">
+          <Table size="small" aria-label="customized table">
             <TableHead>
               <TableRow>
-                <TableCell>배송일시</TableCell>
-                <TableCell>주소</TableCell>
-                <TableCell>메뉴</TableCell>
-                <TableCell>배송현황</TableCell>
+                <StyledTableCell>배송일시</StyledTableCell>
+                <StyledTableCell>주소</StyledTableCell>
+                <StyledTableCell>메뉴</StyledTableCell>
+                <StyledTableCell>배송현황</StyledTableCell>
               </TableRow>
             </TableHead>
             <TableBody>
