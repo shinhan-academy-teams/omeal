23ffad3898b8561e6d2ce220860bdaeecd9273de
KKangBro <417kky@gmail.com>
2023-08-09 19:17:26 +0900

    fix(FE) : 등급 이미지 수정

--- frontend/omeal/src/component/common/Bottom.js
+++ frontend/omeal/src/component/common/Bottom.js
@@ -6,8 +6,7 @@ import HomeIcon from "@mui/icons-material/Home";
 import Diversity3Icon from "@mui/icons-material/Diversity3";
 import MopedIcon from "@mui/icons-material/Moped";
 import EggIcon from "@mui/icons-material/Egg";
-import { useNavigate } from "react-router-dom";
-import eggImg from "../../assets/img/egg2.png";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { MemberGradeState } from "../../recoil/SignInState";
 import eggImg1 from "../../assets/img/egg1.png";
@@ -19,6 +18,19 @@ function Bottom(props) {
   const navi = useNavigate();
   const [value, setValue] = React.useState(0);
   const memberGrade = useRecoilValue(MemberGradeState);
+
+  const location = useLocation();
+  //현재 경로 알아내기
+  const currentPath = location.pathname;
+
+  console.log(currentPath);
+
+  const eggImgs = {
+    날계란: eggImg1,
+    반숙란: eggImg2,
+    완숙란: eggImg3,
+    훈제란: eggImg4,
+  };
 
   const myPage = () => {
     navi("/mypage");
@@ -49,32 +61,36 @@ function Bottom(props) {
         >
           <BottomNavigationAction
             label="홈"
+            id="home"
             icon={<HomeIcon />}
             onClick={main}
           />
           <BottomNavigationAction
             label="커뮤니티"
+            id="community"
             icon={<Diversity3Icon />}
             onClick={omealland}
           />
-
-          {memberGrade === "날계란" ? (
-            <img alt="" src={eggImg1}></img>
-          ) : memberGrade === "반숙란" ? (
-            <img alt="" src={eggImg2}></img>
-          ) : memberGrade === "완숙란" ? (
-            <img alt="" src={eggImg3}></img>
-          ) : (
-            <img alt="" src={eggImg4}></img>
-          )}
-
+          <BottomNavigationAction
+            disabled
+            label=""
+            icon={
+              <img
+                alt="egg grade"
+                width="48px"
+                src={memberGrade ? eggImgs[memberGrade] : eggImgs["날계란"]}
+              />
+            }
+          />
           <BottomNavigationAction
             label="오늘의 밀"
+            id="omeal"
             icon={<MopedIcon />}
             onClick={todayMeal}
           />
           <BottomNavigationAction
             label="마이페이지"
+            id="mypage"
             icon={<EggIcon />}
             onClick={myPage}
           />
