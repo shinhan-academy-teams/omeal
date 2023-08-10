18416b7d1f6b01a1fd83396cd93840d3a3b0701c
Jennorresothie <head7579@gmail.com>
2023-08-10 16:15:27 +0900

    feat(FE) 새로고침해도 recoil 초기화 막기

--- frontend/omeal/src/recoil/SignInState.js
+++ frontend/omeal/src/recoil/SignInState.js
@@ -1,31 +1,40 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 export const SignInState = atom({
   key: "SignInState",
   default: "",
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const MemberGradeState = atom({
   key: "MemberGradeState",
   default: "",
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const MemberNameState = atom({
   key: "MemberNameState",
   default: "",
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const MemberNickState = atom({
   key: "MemberNickState",
   default: "",
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const ContinuousDaysState = atom({
   key: "ContinuousDaysState",
   default: "",
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const SubCheckState = atom({
   key: "SubCheckState",
   default: "",
+  effects_UNSTABLE: [persistAtom],
 });
