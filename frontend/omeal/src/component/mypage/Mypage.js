d80eb9e340ee4ee938854a924e067e1470624cf9
Jennorresothie <head7579@gmail.com>
2023-08-10 17:11:44 +0900

    feat(FE) 구독 정보 알림

--- frontend/omeal/src/component/mypage/Mypage.js
+++ frontend/omeal/src/component/mypage/Mypage.js
@@ -7,25 +7,33 @@ import {
   ContinuousDaysState,
   MemberGradeState,
   MemberNickState,
+  SubCheckState,
 } from "../../recoil/SignInState";
 import eggImg1 from "../../assets/img/egg1.png";
 import eggImg2 from "../../assets/img/egg2.png";
 import eggImg3 from "../../assets/img/egg3.png";
 import eggImg4 from "../../assets/img/egg4.png";
+import Swal from "sweetalert2";
 
 function Mypage(props) {
   //멤버 닉네임 & 구독 개월수
   const memberNick = useRecoilValue(MemberNickState);
   const memberGrade = useRecoilValue(MemberGradeState);
   const continuousDay = useRecoilValue(ContinuousDaysState);
-
-  //console.log(memberGrade);
+  const sub = useRecoilValue(SubCheckState);
 
   const navi = useNavigate();
   const userInfo = () => {
     navi("/user-info");
   };
   const subInfo = () => {
+    if (!sub) {
+      Swal.fire({
+        icon: "warning", // 여기다가 아이콘 종류를 쓰면 됩니다.
+        text: "구독중인 서비스가 없습니다.",
+      });
+      return;
+    }
     navi("/sub-info");
   };
   const cardInfo = () => {
