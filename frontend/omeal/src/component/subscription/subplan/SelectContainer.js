05430e1d60b09a2c7d2bce41dafe9ef00733aa16
djdjdddd <djdj2297@gmail.com>
2023-08-10 17:40:24 +0900

    feat(FE) : 구독신청 中 식사 용기 선택 페이지 구현 완료

--- frontend/omeal/src/component/subscription/subplan/SelectContainer.js
+++ frontend/omeal/src/component/subscription/subplan/SelectContainer.js
@@ -1,4 +1,18 @@
-import { Box, Grid, ToggleButton, ToggleButtonGroup } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  FormControl,
+  FormControlLabel,
+  Grid,
+  Radio,
+  RadioGroup,
+  ToggleButton,
+  ToggleButtonGroup,
+  Typography,
+} from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { ContainerTypeAtom } from "../../../recoil/SubscriptionState";
@@ -8,56 +22,75 @@ import disposableContainer from "../../../assets/img/subscription/disposable_con
 
 function SelectContainer(props) {
   const [containerType, setContainerType] = useRecoilState(ContainerTypeAtom);
-  const [typeList, setTypeList] = useState([]);
+  // const [typeList, setTypeList] = useState([]); // 왜 axios로 받아오는지 몰라서 주석 처리
 
-  const handleChange = (event, newAlignment) => {
-    setContainerType(newAlignment);
+  const [selectedValue, setSelectedValue] = useState(null);
+  const handleCardClick = (value) => {
+    setSelectedValue(value);
+    if (value === "일회용기") {
+      setContainerType(value);
+    } else if (value === "다회용기") {
+      setContainerType(value);
+    }
+    console.log("카드 클릭함 : " + value);
   };
 
-  const imgStyle = {
-    width: "100%",
-    borderRadius: "20px",
-  };
-
-  useEffect(() => {
-    axios
-      .get("/plan-container")
-      .then(function (response) {
-        setTypeList(response.data);
-      })
-      .catch(function (error) {});
-  }, []);
+  // useEffect(() => { // 왜 axios로 받아오는지 몰라서 주석 처리
+  //   axios
+  //     .get("/plan-container")
+  //     .then(function (response) {
+  //       setTypeList(response.data);
+  //     })
+  //     .catch(function (error) {});
+  // }, []);
 
   return (
     <Box sx={{ display: "flex", justifyContent: "center", mb: 2 }}>
       <Grid container spacing={2}>
         <Grid item xs={12}>
-          <ToggleButtonGroup
-            color="primary"
-            value={containerType}
-            exclusive
-            onChange={handleChange}
-            aria-label="Platform"
-            style={{ flexGrow: 1 }}
-          >
-            {typeList.map((value, index) => (
-              <ToggleButton
-                key={index}
-                value={value}
-                aria-label="disposable"
-                style={{ flexGrow: 1 }}
-              >
-                {value}
-              </ToggleButton>
-            ))}
-          </ToggleButtonGroup>
+          <Typography color="primary.main">
+            (미선택 시 일회용기로 드립니다)
+          </Typography>
         </Grid>
         <Grid item xs={1} />
         <Grid item xs={5}>
-          <img alt="" src={multiUseContainer} style={imgStyle}></img>
+          <Card
+            sx={{
+              boxShadow:
+                selectedValue === "일회용기" &&
+                "0px 4px 8px rgba(0.9, 0.9, 0.9, 0.9)",
+            }}
+          >
+            <CardActionArea onClick={() => handleCardClick("일회용기")}>
+              <CardMedia
+                component="img"
+                image={disposableContainer}
+              ></CardMedia>
+              <CardContent>
+                <Typography color="primary" variant="h6">
+                  일회용기
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
         </Grid>
         <Grid item xs={5}>
-          <img alt="" src={disposableContainer} style={imgStyle}></img>
+          <Card
+            sx={{
+              boxShadow:
+                selectedValue === "다회용기" &&
+                "0px 4px 8px rgba(0.9, 0.9, 0.9, 0.9)",
+            }}
+          >
+            <CardActionArea onClick={() => handleCardClick("다회용기")}>
+              <CardMedia component="img" image={multiUseContainer}></CardMedia>
+              <CardContent>
+                <Typography color="primary" variant="h6">
+                  다회용기
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
         </Grid>
         <Grid item xs={1} />
       </Grid>
