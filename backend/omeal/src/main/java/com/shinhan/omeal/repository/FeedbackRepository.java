1858d14334b9411a387b045adb67a203cadbe0d7
KKangBro <417kky@gmail.com>
2023-08-11 13:14:27 +0900

    feat(BE) : 오밀 피드백 읽어오기 및 남기기 구현 완료

--- backend/omeal/src/main/java/com/shinhan/omeal/repository/FeedbackRepository.java
+++ backend/omeal/src/main/java/com/shinhan/omeal/repository/FeedbackRepository.java
@@ -4,12 +4,9 @@ import com.shinhan.omeal.entity.Feedback;
 import com.shinhan.omeal.entity.Members;
 import com.shinhan.omeal.entity.Menu;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.transaction.annotation.Transactional;
 
 public interface FeedbackRepository extends CrudRepository<Feedback, Long> {
 
     public Feedback findByMemberAndMenu(Members member, Menu menu);
 
-    @Transactional
-    public void deleteByMemberAndMenu(Members member, Menu menu);
 }
